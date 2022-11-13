03e6ad0cbb1ec19ce9accc238b2f1d3f8fa669f1
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-11-13 16:20:52 -0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -133,6 +133,10 @@ class PauseSubState extends MusicBeatSubstate
 
 		regenMenu();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+		
+		#if android
+		addVirtualPad(UP_DOWN, A);
+		#end
 	}
 
 	var holdTime:Float = 0;
